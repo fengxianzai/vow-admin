f0b941e3358d5affc9011df8eac02ed998dff3ab
fengxianzai <1617469907@qq.com>
2022-02-23 20:48:30 +0800

    feat: 项目引用iconfont(阿里巴巴矢量图标)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
-import installElementPlus from './plugins/element'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import './style/iconfont.css'
 import './style/index.scss'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -8,25 +8,19 @@
       <!-- 用户名 -->
       <el-form-item pro="username">
         <span class="svg-container">
-          <el-icon>
-            <Avatar />
-          </el-icon>
+          <i class="vow-icon-yonghu"></i>
         </span>
-        <el-input placeholder="username" name="username" type="text" />
+        <el-input placeholder="username" name="username" type="text"></el-input>
       </el-form-item>
 
       <!-- 密码 -->
       <el-form-item pro="password">
         <span class="svg-container">
-          <el-icon>
-            <Avatar />
-          </el-icon>
+          <i class="vow-icon-mima"></i>
         </span>
-        <el-input placeholder="password" name="password" />
+        <el-input placeholder="password" name="password"></el-input>
         <span class="show-pwd">
-          <el-icon>
-            <Avatar />
-          </el-icon>
+          <i class="vow-icon-xianshimima"></i>
         </span>
       </el-form-item>
 
@@ -39,8 +33,81 @@
 </template>
 
 <script>
-import { Avatar } from '@element-plus/icons'
 export default {}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
